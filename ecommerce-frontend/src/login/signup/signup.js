a5713c592c5fb02e1dfdb91c15ab0cdfc3e1ad42
andresdlRoca <andresdelaroca11@gmail.com>
2022-05-01 13:30:09 -0600

    Unused import removed

--- ecommerce-frontend/src/login/signup/signup.js
+++ ecommerce-frontend/src/login/signup/signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { regular_signup } from "./firebaselogin";
-import PasswordChecklist from "react-password-checklist"
 
 
 export default function Signup() {
