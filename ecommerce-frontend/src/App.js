839eeb453a4c9a32f2a31f14da4e36d224c630c5
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-05-10 03:09:16 -0600

    Front
    Se ha agregado la parte de la plantilla de la pagina

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import pp from '../img/pp.jpg';
+import GreenCircle from '../img/greenCir.png';
+import redCirc from '../img/redCirc.png';
+import grayCirc from '../img/grayCirc.png';
 import './style.scss';
 const App = () => (
   <>
-    <body>
-      <div className="all-container">
-        <div id="left-panel">
-          <img src={pp} id="pp"></img>
-          <div className="options-container">
-            <a href="" className="option">
-              Home
-            </a>
-            <a href="" className="option">
-              Chats
-            </a>
-            <a href="" className="option">
-              Settings
-            </a>
-            <a href="" className="option">
-              Logout
-            </a>
+    <div className="container">
+      <div className="leftPanel">
+        <img src={pp} id="pp"></img>
+        <div className="options-container">
+          <a href="" className="option">
+            Home
+          </a>
+          <a href="" className="option">
+            Chats
+          </a>
+          <a href="" className="option">
+            Settings
+          </a>
+          <a href="" className="option">
+            Logout
+          </a>
+        </div>
+      </div>
+      <div className="panel-container">
+        <div id="generalPanel">
+          <div className="items-container">
+            <div className="head">
+              <h1>Panel de Chats</h1>
+              <img className="circle" src={GreenCircle}></img>
+              <div>Activo</div>
+              <img className="circle" src={redCirc}></img>
+              <div>Cerrado</div>
+              <img className="circle" src={grayCirc}></img>
+              <div>Abandonado</div>
+            </div>
           </div>
         </div>
       </div>
-    </body>
+    </div>
   </>
 );
 
