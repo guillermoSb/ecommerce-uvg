94f2e8462953ca44518850ad846b1841dc1d7f67
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-05-10 09:25:25 -0600

    .

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import pp from '../img/pp.jpg';
 import './style.scss';
 const App = () => (
   <>
     <body>
       <div className="all-container">
-        <div className="panel"></div>
+        <div id="left-panel">
+          <img src={pp} id="pp"></img>
+          <div className="options-container">
+            <a href="" className="option">
+              Home
+            </a>
+            <a href="" className="option">
+              Chats
+            </a>
+            <a href="" className="option">
+              Settings
+            </a>
+            <a href="" className="option">
+              Logout
+            </a>
+          </div>
+        </div>
       </div>
     </body>
   </>
