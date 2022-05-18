8e3724f5e2c3e03ed1af3dc194f884d509d8919e
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-17 18:20:31 -0600

    Implemented login branch

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/login";
 import Signup from "./components/signup";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
