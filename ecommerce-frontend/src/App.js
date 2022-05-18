8e7e1692356ed7fed5cd092f78ac7f2b863fd083
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-17 18:25:42 -0600

    only render chat when login has happened

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login";
 import Signup from "./components/signup";
+import ChatBubble from "./componentes/ChatBubble"
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth } from "firebase/auth";
 
 function App() {
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);  // Hook to auth state changes on the app
+  useEffect(() => {
+    if (user) {
+      console.log('User is signed in!', user.uid)
+    }
+  }, [user])
   return (
     <>
       <Router>
@@ -13,6 +23,7 @@ function App() {
           <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </Router>
+      {user ? <ChatBubble /> : ''}
     </>
   );
 }
