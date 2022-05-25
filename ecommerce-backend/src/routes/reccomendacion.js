5769406e7637897dafdca10f60a98c9e79a9c0fb
estuardo8u14 <estuardo8u14@gmail.com>
2022-05-25 08:49:27 -0600

    recomendaciones

--- ecommerce-backend/src/routes/reccomendacion.js
+++ ecommerce-backend/src/routes/reccomendacion.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
-const { getRecomendationByName, getRecomendationByCategory, getItemById } = require("../controllers/reccomendacion");
+const {
+  getRecomendationByName,
+  getRecomendationByCategory,
+  getItemById,
+  getRecomendationByRating,
+  getRecomendationByBrand,
+} = require("../controllers/reccomendacion");
 
 const router = Router();
 
 router.get("/get-nombre/:nombre", getRecomendationByName);
 router.get("/by-category/:categoria", getRecomendationByCategory);
 router.get("/get-item-by-id/:id", getItemById);
+router.get("/get-calificacion/:calificacion", getRecomendationByRating);
+router.get("/get-marca/:marca", getRecomendationByBrand);
 
 module.exports = router;
