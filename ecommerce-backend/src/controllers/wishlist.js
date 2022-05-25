996d9f143845f6ed55a74175a7f033062d7800c1
AxelLeo129 <axelleo129dev@gmail.com>
2022-05-25 00:39:04 -0600

    Whishlist backend.

--- ecommerce-backend/src/controllers/wishlist.js
+++ ecommerce-backend/src/controllers/wishlist.js
@@ -1,31 +1,60 @@
-const {
-  collection,
-  getDocs,
-  addDoc,
-  query,
-  where,
-  updateDoc,
-  doc,
-} = require("firebase/firestore");
+const { collection, getDocs, addDoc, query, where, deleteDoc, doc } = require("firebase/firestore");
 const { db } = require("../firebase");
 
 const getWishlistByUser = async (req, res) => {
   const { uid } = req.params;
   try {
-    const querySnapshot = await getDocs(query(collection(db, "wishlist"), where("usercode", "==", uid)));
-    if(querySnapshot.docs.map((doc) => doc.data()))
-    return res.status(200).send({
-      ok: true,
-      wishlist: querySnapshot.docs.map((doc) => doc.data()),
-    });
+    const querySnapshot = await getDocs(
+      query(collection(db, "wishlist"), where("usercode", "==", uid))
+    );
+    if (querySnapshot.docs.map((doc) => doc.data()))
+      return res.status(200).send({
+        ok: true,
+        wishlist: querySnapshot.docs.map((doc) => doc.data()),
+      });
   } catch (error) {
     return res.status(500).send({
       ok: false,
       errors: ["Algo salió mal."],
     });
   }
-}
+};
+
+const saveDoc = async (req, res) => {
+  try {
+    const { obj } = req.body;
+    addDoc(collection(db, "wishlist"), obj);
+    return res.status(200).send({
+      ok: true,
+      msg: "Agregado a la lista de deseos.",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      ok: true,
+      msg: "Error al guardar.",
+    });
+  }
+};
+
+const removeDoc = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const ref = doc(db, "wishlist", id);
+    deleteDoc(ref);
+    return res.status(200).send({
+      ok: true,
+      msg: "Eliminado de la lista de deseos.",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      ok: true,
+      msg: "Error al eliminar.",
+    });
+  }
+};
 
 module.exports = {
-  getWishlistByUser
-}
+  getWishlistByUser,
+  saveDoc,
+  removeDoc,
+};
--- ecommerce-backend/src/routes/wishlist.js
+++ ecommerce-backend/src/routes/wishlist.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { getWishlistByUser } = require("../controllers/wishlist");
+const { getWishlistByUser, saveDoc, removeDoc } = require("../controllers/wishlist");
 
 const router = Router();
 
 router.get("/by-user/:uid", getWishlistByUser);
+router.post("/save-as-wish", saveDoc);
+router.delete("/remove-from-wish", removeDoc);
 
 module.exports = router;
