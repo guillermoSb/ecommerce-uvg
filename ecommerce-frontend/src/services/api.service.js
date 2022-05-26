8f7bd1bee79867859c0a57158742aca876be6b87
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-26 10:25:56 -0600

    bugfix and block if inactive

--- ecommerce-frontend/src/services/api.service.js
+++ ecommerce-frontend/src/services/api.service.js
@@ -1,67 +1,60 @@
-
-
 const api_url = "http://localhost:8000/api";
 
 /**
- * Iniciar  
- * @param {string} iniciadoPor 
+ * Iniciar
+ * @param {string} iniciadoPor
  */
 export const initChat = async (iniciadoPor) => {
-    const URL = `${api_url}/chats`;
-    const body = { iniciadoPor }
-    const otherPram = {
-
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-
-    }
-    const res = await fetch(URL, otherPram);
-    const data = await res.json();
-    return data;
-}
+  const URL = `${api_url}/chats`;
+  const body = { iniciadoPor };
+  const otherPram = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+  const res = await fetch(URL, otherPram);
+  const data = await res.json();
+  return data;
+};
 
 /**
- * Iniciar  
- * @param {string} iniciadoPor 
+ * Iniciar
+ * @param {string} iniciadoPor
  */
 export const changeState = async (id, state) => {
-    const URL = `${api_url}/chats/state`;
-    const body = { id, estado: state }
-    const otherPram = {
-
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-
-    }
-    const res = await fetch(URL, otherPram);
-    const data = await res.json();
-    return data;
-}
+  const URL = `${api_url}/chats/state`;
+  const body = { id, estado: state };
+  const otherPram = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+  const res = await fetch(URL, otherPram);
+  const data = await res.json();
+  return data;
+};
 
 export const sendingChat = async (uId, cId, uMessage) => {
-    // console.log('sending')
+  if (uMessage && uMessage !== "") {
     const URL = `${api_url}/chats/sendChat`;
     const body = {
-        UserId: uId,
-        chatId: cId,
-        userMessage: uMessage
-    }
+      UserId: uId,
+      chatId: cId,
+      userMessage: uMessage,
+    };
     const otherPram = {
-
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-
-    }
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
     const res = await fetch(URL, otherPram);
     const data = await res.json();
     return data;
-}
+  }
+};
